8a3d0284eb35be908c3d004a00694ac2bc34ca22
Daniel <daniel.griffin@tufts.edu>
2014-10-01 22:27:07 -0400

    updated to use youtube-mp3.org with selenium. Currently 67% success rate. Only gets download urls

--- pull.py
+++ pull.py
@@ -1,5 +1,6 @@
 import gdata.youtube
 import gdata.youtube.service
+from time import sleep
 
 yt_service = gdata.youtube.service.YouTubeService()
 yt_service.ssl = True
@@ -21,6 +22,38 @@ for pid in list_ids:
             videos.append(video.link[0].href.replace("&feature=youtube_gdata", ""))
 
 #order not preserved but ensures if a video will only be downloaded one time
+
 videos = list(set(videos))
 
-print videos        
+sleep(10)
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+mp3url = "http://www.youtube-mp3.org/"
+#Uses firefox browser -- must be installed!
+browser = webdriver.Firefox()
+links = []
+
+print "Videos attempted: {}".format(len(videos))
+count = 0
+for video in videos:
+    try:
+        browser.get(mp3url)
+        iput = browser.find_element_by_id('youtube-url')
+        iput.clear()
+        iput.send_keys(video + Keys.RETURN)
+        sleep(5)
+        anchors = browser.find_elements_by_xpath('//*[@id="dl_link"]/a')
+        for anchor in anchors:
+            link = anchor.get_attribute("href")
+            if link.split('/')[-1][:7] == "get?ab=":
+                links.append(link)
+    except:
+        print video
+        count += 1
+browser.close()
+
+print "Failure rate: {}".format( float(count) / len(videos))
+
+print links
+    
